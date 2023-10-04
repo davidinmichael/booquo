62217883d1815d29d64be9aa623a5cf2988c7429
davidinmichael <davidinmichael@gmail.com>
2023-10-04 10:35:33 +0100

    Books are now paginated up to 50 a page

--- library/views.py
+++ library/views.py
@@ -1,15 +1,23 @@
+from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import permissions
 import requests
 import lxml
 import bs4
 from .models import Book
 from .serializers import *
 
-class Books(APIView):
+# class Books(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     pagination_class = PageNumberPagination
 
-    def get(self, request):
+# class Books(APIView):
+#     def get(self, request):
+
         # Books and info were scrapped with the below information
 
         # books_and_prices = []
@@ -56,8 +64,25 @@ class Books(APIView):
         #     # books_list.append(book_instances)
 
         # Books.objects.bulk_create(books_list)
-        books_data = Book.objects.all()
-        serializer = BookSerializer(books_data, many=True)
+        # books_data = Book.objects.all()
+        # serializer = BookSerializer(books_data, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # return Response(serializer.data, status=status.HTTP_200_OK)
     
+
+class Books(APIView, PageNumberPagination):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def get(self, request):
+        books_data = Book.objects.all()
+        response = self.paginate_queryset(books_data, request, view=self)
+        serializer = BookSerializer(response, many=True)
+
+        data = {
+            "message": "Books retrieved successfully!",
+            "status_code": status.HTTP_200_OK,
+            "books": serializer.data,
+        }
+
+        return self.get_paginated_response(data)
+
+        # return Response(serializer.data, status=status.HTTP_200_OK)
